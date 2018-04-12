54434e71cfc222790395d18b24da70b21ade2a02
Bianca Rosa <me@biancarosa.com.br>
2018-04-12 13:09:22 -0300

    add one more lua resty template example

--- lua-resty-template.lua
+++ lua-resty-template.lua
@@ -1,7 +1,10 @@
 local template = require "resty.template"
--- Using template.new
+-- -- Using template.new
 local view = template.new "view.html"
 view.message = "Hello, World!"
 view:render()
 -- Using template.render
-template.render("view.html", { message = "Hello, World!" })
+template.render("view.html", { message = "Hello, World" })
+-- Using template.render with local string
+local string_template = "{{message}}"
+template.render(string_template, { message = "Hello, World" })
